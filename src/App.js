717302511451ebb53001eba81ea795548f71cb4a
algomachine007 <amaben202phantom@gmail.com>
2024-02-20 07:19:56 +0100

    implementing barrel approach

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
-import { Home } from "./Views";
+import * as View from "./Views";
 
 function App() {
   return (
     <div className="p-10">
       <Router>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<View.Home />} />
         </Routes>
       </Router>
     </div>
