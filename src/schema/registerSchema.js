48c53e80da2d30c3a3875a93530e83dc852cb930
algomachine007 <amaben202phantom@gmail.com>
2024-02-24 09:44:48 +0100

    building the input schema

--- src/schema/registerSchema.js
+++ src/schema/registerSchema.js
@@ -2,6 +2,7 @@ import { date, number, object, string } from "yup";
 
 export const registerSchema = object({
   name: string()
+    .matches(/^ [a-zA-Z_ ]*$/, "FullName can contain underscore")
     .min(2, "Full name must be greater than 2 characters")
     .max(16, "Full name must be less than 16 characters")
     .required(),
