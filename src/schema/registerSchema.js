91fa35ad83ea64939284b37587b1d5854bf1294f
algomachine007 <amaben202phantom@gmail.com>
2024-02-24 09:48:53 +0100

    adding password regex

--- src/schema/registerSchema.js
+++ src/schema/registerSchema.js
@@ -1,4 +1,4 @@
-import { date, number, object, string } from "yup";
+import { number, object, string } from "yup";
 
 export const registerSchema = object({
   name: string()
@@ -6,8 +6,12 @@ export const registerSchema = object({
     .min(2, "Full name must be greater than 2 characters")
     .max(16, "Full name must be less than 16 characters")
     .required(),
-  age: number().required().positive().integer(),
-  email: string().email(),
-  website: string().url().nullable(),
-  createdOn: date().default(() => new Date()),
+  status: number().required().max(64, "Status must be less than 64 chars"),
+  email: string().email().required("Please enter your email address"),
+  password: string()
+    .required("Enter your password")
+    .matches(
+      /^(?=.*\d)(?=.*[A-Z])(?=.*[a-z])(?=.*[^a-zA-Z0-9]).{6,}$/,
+      "Password must contain 1 uppercase letter, 1 lowercase letter, 1 special character and 1 number,",
+    ),
 });
