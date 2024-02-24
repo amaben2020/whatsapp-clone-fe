c3bda9e6acffcf6d693210920db834eb46c6eb0c
algomachine007 <amaben202phantom@gmail.com>
2024-02-24 12:55:30 +0100

    adding clear image capability

--- src/components/Input/Picture.js
+++ src/components/Input/Picture.js
@@ -5,23 +5,31 @@ const Picture = ({ setReadablePicture, setPicture, readablePicture }) => {
   const handlePicture = (e) => {
     try {
       const imageFile = e.target.files[0];
+      if (imageFile) {
+        setPicture(imageFile);
+        setError("");
+      }
 
       const reader = new FileReader(); // asynchronously read the contents of files (or raw data buffers) stored on the user's computer
 
       // max size is greater than 5mb
-      if (imageFile.size > 1024 * 1024 * 5) {
-        setError("File too large");
+      if (imageFile.size > 1024 * 1024 * 2) {
+        setError("File must be less than 5mb");
+        setReadablePicture("");
+        setPicture({});
+        return;
       }
 
       if (imageFile && imageFile.type.match("image.*")) {
         reader.readAsDataURL(imageFile);
         reader.onload = (e) => {
-          console.log("e", e.target);
           setReadablePicture(e.target?.result);
+          setError("");
         };
       }
     } catch (error) {
       console.log(error);
+      setError("Something went wrong");
     }
   };
 
@@ -36,9 +44,19 @@ const Picture = ({ setReadablePicture, setPicture, readablePicture }) => {
           label="Picture (Optional)"
           accept=".png, .jpg, .jpeg"
         />
-        {error.length > 0 && <p> {error}</p>}
+        {error.length > 0 && <p className="text-red-500"> {error}</p>}
         {readablePicture && (
-          <img src={readablePicture} className="w-20 h-20 rounded-lg" alt="" />
+          <div className="relative border">
+            <img
+              src={readablePicture}
+              className="w-[300px] h-[200px] rounded-lg mx-auto shadow-lg"
+              alt="img"
+            />
+
+            <button className="absolute z-10 p-2 rounded-full right-[110px] -top-3 bg-red-300">
+              🗑️
+            </button>
+          </div>
         )}
       </div>
     </div>
