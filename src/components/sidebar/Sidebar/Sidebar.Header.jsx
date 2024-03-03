bfb63f370e904d92cb797b8b8e998f74066ccaab
algomachine007 <amaben202phantom@gmail.com>
2024-03-03 17:51:38 +0100

    creating sidebar header

--- src/components/sidebar/Sidebar/Sidebar.Header.jsx
+++ src/components/sidebar/Sidebar/Sidebar.Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { ContactIcon } from "../../svg";
+import ChatIcon from "../../svg/Chat";
+import CommunityIcon from "../../svg/Community";
 
 const SidebarHeader = () => {
-  return <div>SidebarHeader</div>;
+  return (
+    <div className="flex items-center justify-between p-3 border">
+      <div>
+        <ContactIcon className="w-40 h-20" />
+      </div>
+      <div className="flex items-center gap-4">
+        <CommunityIcon className="text-white fill-white" />
+        <ChatIcon className="text-white fill-white" />
+      </div>
+    </div>
+  );
 };
 
 export default SidebarHeader;
