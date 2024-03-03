f5e8078ef45ced38b209a3d75eb2bb8f96e25e15
algomachine007 <amaben202phantom@gmail.com>
2024-03-03 17:44:50 +0100

    composing sidebar header

--- src/Layout/PageLayout.jsx
+++ src/Layout/PageLayout.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Sidebar from "../components/sidebar/Sidebar/Sidebar";
+import SidebarHeader from "../components/sidebar/Sidebar/Sidebar.Header";
 
 const PageLayout = ({ children }) => {
-  return <div className="min-h-screen p-10 bg-gray-800">{children}</div>;
+  return (
+    <div>
+      <header className="block w-full p-5 text-white bg-black">
+        <nav>LOGOUT</nav>
+      </header>
+      <div className="flex min-h-screen p-10 bg-gray-800 gap-x-6">
+        <aside className="w-[40%] border ">
+          <Sidebar header={<SidebarHeader />} />
+        </aside>
+        <section>{children}</section>
+      </div>
+    </div>
+  );
 };
 
 export default PageLayout;
