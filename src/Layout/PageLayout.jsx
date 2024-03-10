ac83ba5048eee6d283fc88b853c90fcf5fbb8cbd
algomachine007 <amaben202phantom@gmail.com>
2024-03-10 19:23:58 +0100

    building the search functionality

--- src/Layout/PageLayout.jsx
+++ src/Layout/PageLayout.jsx
@@ -1,8 +1,9 @@
 import { yupResolver } from "@hookform/resolvers/yup";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Conversations from "../Views/Conversations";
+import { api } from "../base/api";
 import Input from "../components/Input/Input";
 import Notification from "../components/Notification/Notification";
 import Sidebar from "../components/Sidebar/Sidebar";
@@ -11,15 +12,41 @@ import { logout } from "../redux/features/user/userSlice";
 import { searchSchema } from "../schema/searchSchema";
 
 const PageLayout = ({ children }) => {
+  const { user } = useSelector((state) => state);
   const {
     register,
     handleSubmit,
+    watch,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(searchSchema),
   });
 
+  const searchTerm = watch("searchTerm");
+
+  console.log("searchTerm", searchTerm);
+
   const dispatch = useDispatch();
+
+  const [searchResults, setSearchResults] = useState([]);
+  console.log(searchResults);
+  const handleFetchData = useCallback(async () => {
+    try {
+      const { data } = await api.get(`/user?search=${searchTerm}`, {
+        headers: {
+          Authorization: `Bearer ${user?.user?.token}`,
+        },
+      });
+      console.log(data);
+      setSearchResults(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, [searchTerm, user?.user?.token]);
+
+  useEffect(() => {
+    handleFetchData();
+  }, [handleFetchData]);
 
   return (
     <div>
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React from "react";
+import React, { useState } from "react";
 
 const Input = ({
   register,
@@ -11,24 +11,31 @@ const Input = ({
   className,
   variant,
 }) => {
+  const [showArrow, setShowArrow] = useState(false);
   return (
     <>
       <label htmlFor={label}>{label}</label>
-      <input
-        type={type}
-        className={clsx(
-          error[field]?.message && "border-red-500 border-2",
-          variant === "search" && "relative",
-          "p-3 border rounded-lg accent-black",
-          className,
-        )}
-        placeholder={placeholder}
-        {...register(field, { required: true })}
-      />
 
-      {variant === "search" && (
-        <span className="absolute z-20 text-black top-3 left-2">OK</span>
-      )}
+      <span className={clsx(variant === "search" && "relative")}>
+        <input
+          type={type}
+          className={clsx(
+            error[field]?.message && "border-red-500 border-2",
+
+            "p-3 border rounded-lg accent-black text-black",
+            className,
+          )}
+          placeholder={placeholder}
+          {...register(field, { required: true })}
+        />
+
+        {variant === "search" && (
+          <span className="absolute z-20 text-black transform -translate-y-1/2 top-1/2 left-2">
+            OK
+          </span>
+        )}
+      </span>
+
       {error[field]?.message.length && (
         <p className="my-2 text-red-500">{error[field].message}</p>
       )}
