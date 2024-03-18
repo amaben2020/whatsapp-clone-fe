857f0796d8f3806a2745ad94389be0c801086602
algomachine007 <amaben202phantom@gmail.com>
2024-03-18 16:01:23 +0100

    building chat area

--- src/components/Chat/ChatActions.jsx
+++ src/components/Chat/ChatActions.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { messageBG } from "../../constants/message.background";
+import { AttachmentIcon, EmojiIcon } from "../svg";
 
 const ChatActions = ({ activeConvo }) => {
   return (
     <div>
       <div
-        className="max-h-[calc(100vh-220px)] w-full p-6 overflow-scroll border-2"
+        className="max-h-[calc(100vh-250px)] w-full p-6 overflow-scroll"
         style={{
           background: `no-repeat url(${messageBG})`,
         }}
@@ -13,6 +14,23 @@ const ChatActions = ({ activeConvo }) => {
         {JSON.stringify(activeConvo)}
       </div>
       {/* MESSAGING AREA */}
+      <div className="flex w-full gap-6 px-3 py-1 mt-2">
+        <button>
+          <EmojiIcon className="fill-white" />
+        </button>
+
+        <button>
+          <AttachmentIcon className="fill-white" />
+        </button>
+
+        {/* <CloseIcon /> */}
+
+        <input
+          type="text"
+          placeholder="Type message here..."
+          className="w-2/3 p-2 bg-gray-800 border rounded-md"
+        />
+      </div>
       {/* INPUT */}
       {/* FILES */}
       {/* EMOJIS */}
